b68206ea2b68df58b7bd086a49274af2b7806629
Stefan Bartos <stefan.svinciak@gmail.com>
2024-10-10 17:24:56 +0200

    initial structure with leaflet, config, tsx, biome, ts/node

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
 # iframe-map-generator
+
+This a project for a generation map in iframe.
+
+## Prerequisites
+
+This project requires NodeJS (version 20 or later) and NPM.
+[Node](http://nodejs.org/) and [NPM](https://npmjs.org/) are really easy to install.
+To make sure you have them available on your machine,
+try running the following command.
+
+```sh
+$ npm -v && node -v
+6.4.1
+v8.16.0
+```
+
+**Recommendation**: Use node manager such as https://github.com/Schniz/fnm
+
+
+
+## Installation
+
+**BEFORE YOU INSTALL:** please read the [prerequisites](#prerequisites)
+
+
+To install, run:
+
+```sh
+$ npm install
+```
+
+## Usage
+
+### Running the app
+
+```sh
+$ npm run start
+```
+
+
---
+++ src/index.ts
@@ -0,0 +1,56 @@
+import fs from 'node:fs';
+
+import config from '../config.json';
+
+console.log(config);
+
+function createIframe({
+  markersScript,
+  center = [51, 0],
+  zoom = 13,
+}: { markersScript: string; center?: [number, number]; zoom?: number }) {
+  return `
+    <iframe srcdoc="
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <link rel='stylesheet' href='https://unpkg.com/leaflet@1.9.4/dist/leaflet.css' />
+          <script src='https://unpkg.com/leaflet@1.9.4/dist/leaflet.js'></script>
+          <style>
+            #map { height: 100vh; width: 100%; }
+          </style>
+        </head>
+        <body>
+          <div id='map'></div>
+          <script>
+            var map = L.map('map').setView([${center[0]}, ${center[1]}], ${zoom});
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+              attribution: '&copy; OpenStreetMap contributors'
+            }).addTo(map);
+            ${markersScript}
+          </script>
+        </body>
+      </html>
+    " width="100%" height="500" frameborder="0"></iframe>
+  `;
+}
+
+function run() {
+  const { markers } = config;
+
+  let markersScript = '';
+  if (markers) {
+    markersScript = markers
+      .map(
+        (marker) => `
+        L.marker([${marker.position[0]}, ${marker.position[1]}])
+          .addTo(map)${marker.message ? `.bindPopup("${marker.message}")` : ''};
+      `,
+      )
+      .join('\n');
+  }
+
+  fs.writeFileSync('iframe.html', createIframe({ markersScript }));
+}
+
+run();
